ddddbd8b40c6b2d8155ef2db83a8b2d70ea2f9f2
danish0511 <dsdd0511@gmail.com>
2024-10-16 22:25:16 +0530

    entity structure

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    pass
+
+@dataclass
+class DataValidationArtifact:
+    pass
+
+@dataclass
+class DataTransformationArtifact:
+    pass
+
+@dataclass
+class ModelTrainingArtifact:
+    pass
+
+@dataclass
+class ModelEvaluationArtifact:
+    pass
+
+@dataclass
+class ModelPusherArtifact:
+    pass
+
+@dataclass
+class ClassificationMetricArtifact:
+    pass
+
+
+
+
