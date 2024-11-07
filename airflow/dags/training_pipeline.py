e58daa4b44a2fe0910aeeab43acb06a4eab7f61f
danish0511 <dsdd0511@gmail.com>
2024-11-07 10:35:50 +0530

    workflow

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -15,7 +15,7 @@ with DAG(
     # [END default_args]
     description="network security pipeline",
     schedule_interval="@weekly",
-    start_date=pendulum.datetime(2024, 10, 29, tz="UTC"),
+    start_date=pendulum.datetime(2024, 11, 7, tz="UTC"),
     catchup=False,
     tags=["example"],
 ) as dag:
